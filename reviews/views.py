7640f4c787925242a44b6514d8d8c8c98825677b
Farzan <farzan@OFD001M6014.local>
2025-12-11 01:21:20 +0400

    adding form...

--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,21 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 def review(request):
-    return render(request, 'reviews/review.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+
+        if username == '':
+            return render(request, 'reviews/review.html', {
+                'has_error': True,
+            })
+
+        print(username)
+        return HttpResponseRedirect('/thank-you')
+
+    return render(request, 'reviews/review.html', {
+        'has_error': False,
+    })
+
+def thank_you(request):
+    return render(request, 'reviews/thank_you.html')
