b6bc18c05bd8d4c78d83d535671aa1a167549f22
Farzan <farzan@OFD001M6014.local>
2025-12-11 20:47:56 +0400

    work on uploading files

---
+++ profiles/views.py
@@ -0,0 +1,9 @@
+from django.views.generic.edit import CreateView
+
+from profiles.models import UserProfile
+
+class CreateProfileView(CreateView):
+    template_name = 'profiles/create_profile.html'
+    model = UserProfile
+    fields = '__all__'
+    success_url = '/profiles'
--- pyfeedback/urls.py
+++ pyfeedback/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('reviews.urls')),
+    path('profiles/', include('profiles.urls')),
 ]
