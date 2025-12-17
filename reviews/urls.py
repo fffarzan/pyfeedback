67ceb6117121a6a4b50549d447b870560de41406
Farzan <farzan@OFD001M6014.local>
2025-12-17 19:43:16 +0400

    add session

--- reviews/urls.py
+++ reviews/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.NewReviewView.as_view()),
     path('thank-you', views.ThankyYouView.as_view()),
     path('reviews', views.ReviewListView.as_view()),
+    path('reviews/favorite', views.AddFavoriteView.as_view()),
     path('review-detail/<int:pk>', views.ReviewDetail.as_view())
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, View
 from django.views.generic.base import TemplateView
 
 from reviews.forms import ReviewForm
@@ -33,3 +32,16 @@ class ReviewDetail(DetailView):
     template_name = 'reviews/review_detail.html'
     model = Review
     context_object_name = 'review_detail'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        loaded_review = self.object
+        favorite_id = self.request.session.get('favorite_review')
+        context['is_favorite'] = favorite_id == str(loaded_review.id)
+
+class AddFavoriteView(View):
+    def post(self, request):
+        review_id = request.POST['review_id']
+        request.session['favorite_review'] = review_id
+
+        return HttpResponseRedirect('/reviews' + review_id)
